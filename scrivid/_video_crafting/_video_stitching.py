012a3782eb6c98b98948519332fce6e00cbb7eb9
TNTMaster370 <trystynmcote@gmail.com>
2026-01-25 13:15:42 -0700

    Removed `ffmpeg-python` dependency and attempted to fix tox issues;

--- scrivid/_video_crafting/_video_stitching.py
+++ scrivid/_video_crafting/_video_stitching.py
@@ -1,41 +1,32 @@
-from .. import errors
+from scrivid import errors
 
 import os
+import subprocess
 
-import ffmpeg
 
-
-def _concatenate(*, input_file, input_settings, output_file, output_settings):
+def _concatenate(command):
     try:
-        (
-            ffmpeg
-            .input(input_file, **input_settings)
-            .output(output_file, **output_settings)
-            .run(quiet=True)
-        )
-    except ffmpeg._run.Error as exc:
+        subprocess.run(command, capture_output=True)
+    except subprocess.SubprocessError as exc:
         raise errors.InternalErrorFromFFMPEG(exc, exc.stdout, exc.stderr)
 
 
-def stitch_video(temporary_directory, metadata):
+def stitch_video(temporary_directory, metadata, video_length):
     input_file = os.path.join(temporary_directory, "%06d.png")
     output_file = str(metadata.save_location / f"{metadata.video_name}.mp4")
     dimensions = f"{metadata.window_width}x{metadata.window_height}"
 
     # I honest to god could not tell you how I figured this out. I just
     # couldn't figure out how to make a stable result for the life of me.
-    _concatenate(
-        input_file=input_file, 
-        input_settings={
-            "f": "image2",
-            "pattern_type": "sequence",
-            "r": metadata.frame_rate,
-        },
-        output_file=output_file,
-        output_settings={
-            "b:v": "4M",
-            "vcodec": "libx264",
-            "pix_fmt": "yuv420p",
-            "s": dimensions
-        }
-    )
+    command = [
+        "ffmpeg",
+        "-framerate", str(metadata.frame_rate),  # INPUT SETTINGS
+        "-i", str(input_file),
+        "-b:v", "4M",  # # # # # # # # # # # # # # OUTPUT SETTINGS
+        "-vcodec", "libx264",
+        "-pix_fmt", "yuv420p",
+        "-s", dimensions,
+        output_file
+    ]
+
+    _concatenate(command)
